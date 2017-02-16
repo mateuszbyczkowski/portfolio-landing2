63c184936001497e68aeb538b361c60a05bd9d04
Mateusz Byczkowski <byczkowski38@gmail.com>
2017-02-16 15:54:21 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(window).on('load', function() { 
+    $('#page-top').fadeIn(2000);
+    $('.presentation-role').delay(2000).fadeIn(1000);
+    $('#scroll-button').delay(2100).fadeIn(1000);
+    $('#nav-fade').delay(2100).fadeIn(1000);
+ });
+
+ 
---
+++ js/scrolling.js
@@ -0,0 +1,57 @@
+//slide sub-pages on mouse-scroll feature
+$(document).ready(function () {
+    var divs = $('.slide');
+    var dir = 'up'; // wheel scroll direction
+    var div = 0; // current div
+    $(document.body).on('DOMMouseScroll mousewheel', function (e) {
+        if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
+            dir = 'down';
+        } else {
+            dir = 'up';
+        }
+        // find currently visible div :
+        div = -1;
+        divs.each(function(i){
+            if (div<0 && ($(this).offset().top >= $(window).scrollTop())) {
+                div = i;
+            }
+        });
+        if (dir == 'up' && div > 0) {
+            div--;
+        }
+        if (dir == 'down' && div < divs.length) {
+            div++;
+        }
+        
+        $('html,body').stop().animate({
+            scrollTop: divs.eq(div).offset().top
+        }, 1000, 'easeInOutExpo');
+        $('#page-top').css({ opacity: 1});
+        return false;
+    });
+    $(window).resize(function () {
+        $('html,body').scrollTop(divs.eq(div).offset().top);
+    });
+});
+
+//collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//page scrolling feature
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+        
